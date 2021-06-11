c8ef7b09e0727c478831e9ce4d24fcb26ecba380
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-06-11 10:01:52 -0700

    Start UI logic and show it on the HTML page.

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,36 @@ function beepBoop(number) {
   for (let i = number; i >= 0; i--) {
     inputtedNum.unshift(i);
   }
-  let stringOutputNum = inputtedNum.toString();
-  let stringOutNumSplit = stringOutputNum.split(",");
+  const stringOutputNum = inputtedNum.toString();
+  const stringOutNumSplit = stringOutputNum.split(",");
   let newArray = []
   stringOutNumSplit.forEach(function(string) {
     if (string.includes(3)) {
-      let mostImportantException = string.replace(/\d+/g, "Won't you be my neighbor?");
+      let mostImportantException = string.replace(/\d+/g, ' "Won\'t you be my neighbor?"');
       newArray.push(mostImportantException);
     } else if (string.includes(2)) {
-      let mostImportantException = string.replace(/\d+/g, "Boop");
+      let mostImportantException = string.replace(/\d+/g, ' "Boop"');
       newArray.push(mostImportantException);
     } else if (string.includes(1)) {
-      let mostImportantException = string.replace(/\d+/g, "Beep!");
+      let mostImportantException = string.replace(/\d+/g, ' "Beep!"');
       newArray.push(mostImportantException);
     } else {
-      newArray.push(string);
+      newArray.push(" " + string);
     }
   });
   console.log(newArray);
+  return newArray;
 };
 
-
 //UI Logic
+$(document).ready(function() {
+  $("#roboger").submit(function() {
+    event.preventDefault();
+    const enter = $("input#number").val();
+    const mrRobogerSays = beepBoop(enter);
 
+    $("#output").text(mrRobogerSays);
+  });
 
-// $(document).ready(function() {
-// });
+});
 
