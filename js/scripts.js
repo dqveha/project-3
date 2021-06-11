21581f2a4b3f6243fc2169dd5575226ca0067108
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-06-11 10:46:36 -0700

    WIP: Working on creating a button to reverse output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-//Business Logic
-
 function beepBoop(number) {
   let inputtedNum = [];
   for (let i = number; i >= 0; i--) {
@@ -19,21 +17,48 @@ function beepBoop(number) {
       let mostImportantException = string.replace(/\d+/g, ' "Beep!"');
       newArray.push(mostImportantException);
     } else {
-      newArray.push(" " + string);
+      newArray.push(" " + "\"" + string + "\"");
     }
   });
   console.log(newArray);
-  return newArray;
+  return newArray + " . . .";
 };
 
-//UI Logic
+function reverseBoopBeep(number) {
+  let inputtedNum = [];
+  for (let i = number; i >= 0; i--) {
+    inputtedNum.push(i);
+  }
+  const stringOutputNum = inputtedNum.toString();
+  const stringOutNumSplit = stringOutputNum.split(",");
+  let reverseNewArray = []
+  stringOutNumSplit.forEach(function(string) {
+    if (string.includes(3)) {
+      let mostImportantException = string.replace(/\d+/g, ' "Won\'t you be my neighbor?"');
+      reverseNewArray.push(mostImportantException);
+    } else if (string.includes(2)) {
+      let mostImportantException = string.replace(/\d+/g, ' "Boop"');
+      reverseNewArray.push(mostImportantException);
+    } else if (string.includes(1)) {
+      let mostImportantException = string.replace(/\d+/g, ' "Beep!"');
+      reverseNewArray.push(mostImportantException);
+    } else {
+      reverseNewArray.push(" " + "\"" + string + "\"");
+    }
+  });
+  return newArray + " . . .";
+};
+
+
 $(document).ready(function() {
   $("#roboger").submit(function() {
     event.preventDefault();
     const enter = $("input#number").val();
     const mrRobogerSays = beepBoop(enter);
-
+    const reverseMrRoboger = reverseBoopBeep(enter);
+    console.log(reverseMrRoboger);
     $("#output").text(mrRobogerSays);
+    $("#reverseOutput").text(reverseMrRoboger);
   });
 
 });
